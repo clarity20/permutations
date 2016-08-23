2648f8bd0510bcaf85d93cef49e7a0b2f8af8100
Michael Wood <mawood20@gmail.com>
2016-08-23 00:13:31 -0400

    Cleaned up comments

--- permutations.cpp
+++ permutations.cpp
@@ -5,9 +5,11 @@
 
 using namespace std;
 
-void generateAllPermutations(vector< vector<int> > &vec, int size);
+void generateAllPermutations(vector< vector<int> > &vec, int n);
 
-// How to recurse on this for vecs of vecs???
+///////////////////////////////////////////////////////////
+// Helper functions for I/O
+///////////////////////////////////////////////////////////
 template <typename T>
 void stream_data(const T &data)
 {
@@ -18,25 +20,23 @@ template <typename T>
 ostream &operator <<(ostream & stream, const vector<T>& vec)
 {
 	for_each(vec.begin(), vec.end(), stream_data<T>);
-//cout << endl << "Finished special streaming operator..." << endl;
 	return stream;
 }
 
 template <typename T>
 void printVector(const vector<T> &vec)
 {
-//cout << "    Inside printVector...\n";
 	typename vector<T>::const_iterator iter;
-//cout << "    Printing... vec len = " << vec.size() << endl;
-//begin = 0th elt
-//end = 1 beyond last elt
 	for(iter = vec.begin(); iter < vec.end(); iter++)
 	{
-//cout << "    Printing one..." << endl;
 		cout << "    " << *iter << endl;
 	}
 	return;
 }
+
+///////////////////////////////////////////////////////////
+// Entry point
+///////////////////////////////////////////////////////////
 
 #ifdef _BUILD_EXE
 int main(int argc, char *argv[])
@@ -52,28 +52,31 @@ int main(int argc, char *argv[])
 	vector< vector<int> > perms;
 	
 	generateAllPermutations(perms, permSize);
-cout << "Total number of perms is " << perms.size() << endl;
-cout << "Length of each is " << perms[0].size() << endl;
 	printVector(perms);
-//cout << "All printed!\n";
 	return 0;
 }
 #endif
 
+// TODO: This should be class-scope state. Avoid globals.
 int g_compareAgainst;
 bool isGreater(int value)
 {
-//cout << "Comparing avail " << value << " against curr val " << g_compareAgainst << endl; 
 	return (value > g_compareAgainst);
 }
 
 int currentInteger(0);
 int nextInteger() { return ++currentInteger; }
 
-void generateAllPermutations(vector< vector<int> > &vec, int size)
+////////////////////////////////////////////////////////////////////////
+// function generateAllPermutations(vec, n)
+// Populates "vec" with the n! permutations of size n in ascending lexicographical order 
+////////////////////////////////////////////////////////////////////////
+
+void generateAllPermutations(vector< vector<int> > &vec, int n)
 {
-	vector<int> currentPerm(size);
-	generate_n(currentPerm.begin(), size, nextInteger);
+	// Initialize with the very first permutation
+	vector<int> currentPerm(n);
+	generate_n(currentPerm.begin(), n, nextInteger);
 	vec.push_back(currentPerm);
 
 	vector<int>::reverse_iterator backwardWalker = currentPerm.rbegin();
@@ -84,12 +87,9 @@ void generateAllPermutations(vector< vector<int> > &vec, int size)
 	while (true)
 	{
 		// Record the values we observe while walking backwards
-//cout << "prev digit inspected: " << *backwardWalker << endl;
 		// Initialize with the very last digit of the previous permutation
 		availableValues.push_back(*backwardWalker);
 		backwardWalker++;
-//cout << "next digit to inspect: " << *backwardWalker << endl;
-//cout << "now available: " << availableValues.back() << " num available: " << availableValues.size() << endl;
 		// Termination condition = inspected all digits and not even the first is increasable
 		if (backwardWalker == stoppingPoint)
 			break;
@@ -98,7 +98,7 @@ void generateAllPermutations(vector< vector<int> > &vec, int size)
 		if (*backwardWalker < availableValues.back())
 		{
 			// Swap in the smallest available element > the replacee.
-			// Since they're in ascending order we need just a linear search.
+			// Since they're in ascending order a linear search is acceptable.
 			int swapInValue;
 			vector<int>::iterator smallestGreaterIt;
 // Thread-dangerous!!!
@@ -108,19 +108,15 @@ void generateAllPermutations(vector< vector<int> > &vec, int size)
 			int smallestGreater = *smallestGreaterIt;
 // up to here.
 
-			// Step 1: Swap the increasable digit (*backwardWalker) and its next-greater
+			// Step 1: Swap the increasable digit (*backwardWalker) with its next-greater
 			//         in-place in the permutation
 			*smallestGreaterIt = *backwardWalker; // change the entry, don't append
 			*backwardWalker = smallestGreater;
 
 			// Step 2: Write down the available values in-place within the permutation,
 			//         in stored order to reverse them.
-
 			copy(availableValues.begin(), availableValues.end(),
 			     currentPerm.end()-availableValues.size());
-
-//cout << "new perm, length " << currentPerm.size() << ": " << currentPerm[0];
-//cout << " " << currentPerm[1] << " " << currentPerm[2] << endl << endl;
 
 			// We have produced the next permutation. Save it and clean up.
 			vec.push_back(currentPerm);
